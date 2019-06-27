34cfb987f963b093cc626c032fc212c78ac49db8
teldawyj <teldawyj@gmail.com>
2019-06-27 09:28:55 +0800

    Update read_xls.py

--- read_xls.py
+++ read_xls.py
@@ -7,6 +7,11 @@ class Xls:
         self.file = rd.open_workbook(filename=file)
         self.sheet = self.file.sheet_by_index(sheet_num-1)
         self.data = []
+        self.data_t = []
         print("n_row = ", self.sheet.nrows, "\nn_col = ", self.sheet.ncols)
-        self.data = self.sheet._cell_values
-        self.type = self.sheet._cell_types
+        self.sheet_data = self.sheet._cell_values
+        # self.type = self.sheet._cell_types
+        for i in range(self.sheet.nrows):
+            for j in range(self.sheet.ncols):
+                self.data[i][j] = self.sheet_data[i][j]
+                self.data_t[i][j] = self.sheet_data[j][i]
